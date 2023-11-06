70ef5eec9958fd116fda64d05d1b651ec43fc2bd
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-06 20:41:43 +0100

    feat: lazy-img done

--- helloWorld/src/main.ts
+++ helloWorld/src/main.ts
@@ -1,5 +1,31 @@
 import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
- <h1>Hello There</h1>
-`;
+class HelloWorld extends HTMLElement {
+  // constructor() {
+  //   super();
+  // }
+
+  connectedCallback(): void {
+    console.log('He entrado en el DOM');
+  }
+
+  disconnectedCallback(): void {
+    console.log('He salido del DOM');
+  }
+
+  get observedAttributes(): [string, string] {
+    return ['a', 'b'];
+  }
+
+  attributeChangedCallback(
+    atributeName: string,
+    oldValue: string,
+    newValue: string
+  ): void {
+    console.log(
+      `Soy el atributo ${atributeName} y he cambiado de ${oldValue} a ${newValue}`
+    );
+  }
+}
+
+window.customElements.define('hello-world', HelloWorld);
