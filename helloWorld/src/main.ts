40a5fe8cd09ee4a631eb819e258dd51e417d26c5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-06 21:21:21 +0100

    feat: main.ts

--- helloWorld/src/main.ts
+++ helloWorld/src/main.ts
@@ -1,9 +1,9 @@
 import './style.css';
 
 class HelloWorld extends HTMLElement {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+  }
 
   connectedCallback(): void {
     console.log('He entrado en el DOM');
@@ -13,7 +13,7 @@ class HelloWorld extends HTMLElement {
     console.log('He salido del DOM');
   }
 
-  get observedAttributes(): [string, string] {
+  static get observedAttributes(): [string, string] {
     return ['a', 'b'];
   }
 
@@ -28,4 +28,24 @@ class HelloWorld extends HTMLElement {
   }
 }
 
+class LazyImage extends HTMLElement {
+  constructor(public src: string) {
+    super();
+    this.src = this.getAttribute('src')!;
+  }
+
+  connectedCallback(): void {
+    const template = document.querySelector('template');
+    const templateContent = template!.content.cloneNode(true);
+    const img = (templateContent as HTMLElement).querySelector('img');
+    img!.src = this.src;
+
+    this.appendChild(templateContent);
+  }
+
+  static get observedAttributes(): string[] {
+    return ['src'];
+  }
+}
 window.customElements.define('hello-world', HelloWorld);
+window.customElements.define('lazy-img', LazyImage);
